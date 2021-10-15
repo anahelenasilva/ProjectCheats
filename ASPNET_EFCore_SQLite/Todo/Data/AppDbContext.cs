893d20de30cf7b13097bbd7e643f3faa75a99619
Ana Helena <ahelena-magit@vortx.com.br>
2021-10-15 08:48:48 -0300

    chore: add ASPNET_EFCore_SQLite project

---
+++ ASPNET_EFCore_SQLite/Todo/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
